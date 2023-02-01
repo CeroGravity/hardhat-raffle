124350bf604e4c2370f322e5513277294020cb33
InsidTrifl <InsidTrifl@proton.me>
2023-02-01 12:47:11 +0600

    staging test passed || changed callbackGasLimit

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -7,14 +7,14 @@ const networkConfig = {
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 150 gwei Key Hash
         subscriptionId: "9125",
-        callbackGasLimit: "5000000",
+        callbackGasLimit: "500000",
         interval: "30",
     },
     31337: {
         name: "hardhat",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-        callbackGasLimit: "5000000",
+        callbackGasLimit: "500000",
         interval: "30",
     },
 }
